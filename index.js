4e943eb1518cfa57f007220778703a0a4cdceaa4
Hoangpersievn <hoangpersievn@gmail.com>
2019-05-14 15:04:52 +0700

    add practice 1

---
+++ index.js
@@ -0,0 +1,9 @@
+function reverseString( str ){
+
+    let arrayString = str.split("");
+    let arrayStingReverse = arrayString.reverse();
+    return arrayStingReverse.join("");
+
+}
+console.log(reverseString('i love you'));
+console.log("are you ok");
